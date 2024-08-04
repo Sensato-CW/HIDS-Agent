279ba037f2e8a1b3b8c95c8aae4e3c4bd4f1ee3e
Sensato-CW <jbuice@gocloudwave.com>
2024-08-04 17:52:27 -0400

    Update CloudWave HIDS Installer.sh

--- Install Script/CloudWave HIDS Installer.sh
+++ Install Script/CloudWave HIDS Installer.sh
@@ -4,7 +4,6 @@
 OSSEC_DIR="/var/ossec"
 CSV_URL="https://raw.githubusercontent.com/Sensato-CW/HIDS-Agent/main/Install%20Script/HIDS%20Keys.csv"
 CSV_PATH="/tmp/HIDS_Keys.csv"
-PRELOADED_VARS_PATH="ossec-hids-master/etc/preloaded-vars.conf"
 
 # Function to ensure all dependencies are installed
 ensure_dependencies() {
@@ -119,7 +118,7 @@ create_preloaded_vars() {
     local server_ip="$1"
     local key="$2"
     echo "Creating preloaded-vars.conf..."
-    cat << EOF > "$PRELOADED_VARS_PATH"
+    cat << EOF > "ossec-hids-master/etc/preloaded-vars.conf"
 USER_LANGUAGE="en"
 USER_NO_STOP="y"
 USER_INSTALL_TYPE="agent"
@@ -133,9 +132,9 @@ USER_UPDATE="n"
 EOF
 
     # Ensure the configuration file is readable
-    sudo chmod 644 "$PRELOADED_VARS_PATH"
+    sudo chmod 644 "ossec-hids-master/etc/preloaded-vars.conf"
     echo "Preloaded vars file content:"
-    cat "$PRELOADED_VARS_PATH"
+    cat "ossec-hids-master/etc/preloaded-vars.conf"
 }
 
 # Function to download and extract the latest OSSEC version
@@ -157,12 +156,7 @@ find_and_cd_ossec() {
 install_ossec() {
     echo "Installing OSSEC..."
     sudo ./install.sh -q
-    if [ -f "/var/ossec/bin/ossec-control" ]; then
-        sudo /var/ossec/bin/ossec-control start
-    else
-        echo "/var/ossec/bin/ossec-control not found. Installation aborted."
-        exit 1
-    fi
+    sudo /var/ossec/bin/ossec-control start
     echo "OSSEC installation completed."
 }
 
