217b062a50477e5bd5cedb119605baf64622e2e0
Sensato-CW <jbuice@gocloudwave.com>
2024-08-05 08:44:48 -0400

    Update CloudWave HIDS Installer.sh

--- Install Script/CloudWave HIDS Installer.sh
+++ Install Script/CloudWave HIDS Installer.sh
@@ -147,18 +147,15 @@ create_client_keys() {
     local encoded_key="$1"
 
     echo "Creating client.keys file..."
-    echo "Encoded key (before trimming): '$encoded_key'"  # Debug line to show the key before trimming
-
-    # Trim any leading or trailing whitespace
-    encoded_key=$(echo "$encoded_key" | awk '{$1=$1;print}')
-    echo "Encoded key (after trimming): '$encoded_key'"  # Debug line to show the key after trimming
+    echo "Encoded key received: '$encoded_key'"  # Debug line to show the received key
 
     # Decode the base64 key and write directly to the client.keys file
-    echo -n "$encoded_key" | base64 -d | sudo tee /var/ossec/etc/client.keys > /dev/null
+    echo -n "$encoded_key" | base64 --decode | sudo tee /var/ossec/etc/client.keys > /dev/null
 
     echo "client.keys file created with content:"
     sudo cat /var/ossec/etc/client.keys
 }
+
 
 # Function to install OSSEC using the preloaded-vars.conf for unattended installation
 install_ossec() {
