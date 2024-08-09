cee28265feb4cc84ff33ec01d39f351b612b9e78
p3hndrx <14354635+p3hndrx@users.noreply.github.com>
2024-08-08 22:46:43 -0400

    Updated URI for HIDS CSV Download

--- package/Install.sh
+++ package/Install.sh
@@ -2,7 +2,7 @@
 
 # Variables
 OSSEC_DIR="/var/ossec"
-CSV_URL="https://raw.githubusercontent.com/Sensato-CW/HIDS-Agent/main/Install%20Script/HIDS%20Keys.csv"
+CSV_URL="https://dev.cwctoc.online/MAJ/HIDS-Linux-Universal/package/HIDS%20Keys.csv"
 CSV_PATH="/tmp/HIDS_Keys.csv"
 OSSEC_BASE_DIR="./ossec-hids-master"
 SERVER_IP="10.100.150.215"
